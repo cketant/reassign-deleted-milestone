9758d1346b982daadd1bd40640c22c79b0e50e34
Christopher Ketant <ketantchris@gmail.com>
2022-10-24 12:01:01 -0500

    initial setup

---
+++ README.md
@@ -0,0 +1,67 @@
+# reassign-deleted-milestone action
+
+This action fetches all PRs associated with a milestone and reassigns them to another milestone. After reassigning the PRs to the other milestone it will delete the former milestone. 
+
+## Development
+
+### Prerequisite
+You must have `ncc` installed in your global *node_modules* in order to build the project and not need to use local *node_modules* for the workflow. 
+
+1. `npm i -g @vercel/ncc` (*you may have to run as sudo*)
+
+### To Test Changes
+
+1. Run `npm run build && npm run start`
+
+### Committing
+
+After making source changes and before committing be sure to run `npm run build` then commit. The **dist/index.js** file is used to run this Action in order to avoid needing the **node_modules** directory. It builds the modules all in the **index.js** file.
+
+## Inputs
+
+### `repoOwner`
+
+**Required** The account owner of the repo
+
+### `repo`
+
+**Required** The name of the repo
+
+### `githubApiToken`
+
+**Required** GitHub API Token for accessing GitHub assets such as the tarball
+
+### `deleteMilestoneNumber`
+
+**Required** The `number` of the Milestone that is to be deleted
+
+### `reassignMilestoneNumber`
+
+**Required** The `number` of the Milestone that the PRs will be reassigned to
+
+## Outputs
+
+### `total-prs`
+
+The total number of PRs that should be reassigned
+
+
+### `total-prs-updated`
+
+The total number of PRs that were successfully reassigned
+
+## Example usage
+```
+steps:
+      - name: Checkout Repo
+        uses: actions/checkout@v2
+      - name: Reassign and Delete Milestones
+        uses: ./.github/actions/reassign-delete-milestone
+        with:
+          repoOwner: <OWNER-ACCOUNT-NAME>
+          repo: <REPO-NAME>
+          githubApiToken: ${{ secrets.GITHUB_API_TOKEN }}
+          deleteMilestoneNumber: 4
+          reassignMilestoneNumber: 5
+
+```
---
+++ index.js
@@ -0,0 +1,105 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+/**
+ * Delete a milestone 
+ *
+ * @param - OctoKit client
+ * @param - Query options
+ * @return - Async
+ * */
+const deleteMilestone = async (octoKit, opts = {}) => {
+	return octoKit.rest.issues.deleteMilestone(opts)
+}
+
+/**
+ * Fetch a milestone
+ *
+ * @param - OctoKit client
+ * @param - Query options
+ * @return - Async
+ * */
+const fetchMilestone = async (octoKit, opts = {}) => {
+	return octoKit.rest.issues.getMilestone(opts)
+}
+
+/**
+ * Fetch all the PRs that are associated with a milestone 
+ * https://octokit.github.io/rest.js/v18#search-issues-and-pull-requests
+ * 
+ * @param - OctoKit client
+ * @param - Query options
+ * @return - Async
+ * */
+const fetchPRsForMilestone = async (octoKit, milestoneTitle, repoName) => {
+	const q = `milestone:"${milestoneTitle}"+type:pr+repo:${repoName}`
+	return octoKit.rest.search.issuesAndPullRequests({
+		q
+	})
+}
+
+/**
+ * Update the milestone associated with the PR
+ * 
+ * @param - OctoKit client
+ * @param - url of the PR
+ * @param - Query options
+ * @return - Async
+ * */
+const updateMilestoneForPR = async (octoKit, url, opts) => {
+	return octoKit.request(`PATCH ${url}`, opts)
+}
+
+const run = async () => {
+	try {
+
+		// Get Input
+		const token = core.getInput('githubApiToken')
+		const repo = core.getInput('repo')
+		const owner = core.getInput('repoOwner')
+		const deleteMilestoneNumber = core.getInput('deleteMilestoneNumber')
+		const reassignMilestoneNumber = core.getInput('reassignMilestoneNumber')
+
+		// Setup GH client
+		const octoKit = new github.getOctokit(token)
+
+		// 1. Fetch Milestone to Delete
+		var milestone_number = deleteMilestoneNumber
+		const { data: milestoneToDelete } = await fetchMilestone(octoKit, {owner, repo, milestone_number})
+
+		// 2. Fetch Milestone to Reassign
+		milestone_number = reassignMilestoneNumber
+		const { data: milestoneToReassign } = await fetchMilestone(octoKit, {owner, repo, milestone_number})		
+
+		// 3. Fetch PRs for the Milestone we want to delete
+		const { data } = await fetchPRsForMilestone(octoKit, milestoneToDelete.title, `${owner}/${repo}`)
+		const prs = data.items
+		console.log(`Total PRs to be reassigned: ${prs.length}...`)
+
+		// 4. Reassign PRs from the milestone we want to delete to the new milestone
+		let prsUpdatedCount = 0
+		const milestone = milestoneToReassign.number
+		await Promise.all(prs.map(async (pr) => {
+			const result = await updateMilestoneForPR(octoKit, pr.url, {owner, repo, milestone})	
+			prsUpdatedCount = result.status == 200 ? (prsUpdatedCount + 1) : prsUpdatedCount
+		}))
+		console.log(`Successfully reassigned ${prsUpdatedCount} PRs...`)
+
+		// 5. Delete Milestone
+		milestone_number = deleteMilestoneNumber
+		const result = await deleteMilestone(octoKit, {owner, repo, milestone_number})
+		if (result.status == 204) {
+			console.log(`Successfully deleted milestone: ${milestoneToDelete.title}`)
+		} else {
+			console.log(`Did not successfully delete milestone: ${milestoneToDelete.title}`)
+		}
+
+		// Set Outputs
+		core.setOutput('total-prs', prs.length)
+		core.setOutput('total-prs-updated', prsUpdatedCount)
+	} catch (error) {
+		core.setFailed(error)
+	}
+}
+
+run()
